c26f9a9f412b0ffc1d39160bbb2ec91672f32fa7
suwardhan <suwardhan@gmail.com>
2017-08-28 19:29:47 +0530

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,39 @@
+THEME: Gifted - Free Bootstrap Theme
+VERSION: 1.1  (Last Update: Aug. 21,2017)
+AUTHOR: ProBootstrap.com
+AUTHOR URI: https://probootstrap.com/
+Twitter: https://twitter.com/probootstrap
+Facebook: https://facebook.com/probootstrap
+
+
+CREDITS:
+
+Bootstrap
+http://getbootstrap.com/
+
+jQuery
+http://jquery.com/
+
+jQuery Easing
+http://gsgd.co.uk/sandbox/jquery/easing/
+
+HTML5Shiv
+https://github.com/aFarkas/html5shiv
+
+Google Fonts
+https://www.google.com/fonts/
+
+Icomoon
+https://icomoon.io/app/
+
+Respond JS
+https://github.com/scottjehl/Respond/blob/master/LICENSE-MIT
+
+animate.css
+http://daneden.me/animate
+
+jQuery Waypoint
+https://github.com/imakewebthings/waypoints/blog/master/licenses.txt
+
+Demo Images
+https://unsplash.com
---
+++ js/main.js
@@ -0,0 +1,147 @@
+(function() {
+	'use strict';
+
+	/*----------------------------------------
+		Detect Mobile
+	----------------------------------------*/
+	var isMobile = {
+		Android: function() {
+			return navigator.userAgent.match(/Android/i);
+		},
+			BlackBerry: function() {
+			return navigator.userAgent.match(/BlackBerry/i);
+		},
+			iOS: function() {
+			return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+		},
+			Opera: function() {
+			return navigator.userAgent.match(/Opera Mini/i);
+		},
+			Windows: function() {
+			return navigator.userAgent.match(/IEMobile/i);
+		},
+			any: function() {
+			return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+		}
+	};
+
+
+	var magnificPopupControl = function() {
+
+
+		$('.image-popup').magnificPopup({
+			type: 'image',
+			removalDelay: 300,
+			mainClass: 'mfp-with-zoom',
+			gallery:{
+				enabled:true
+			},
+			zoom: {
+				enabled: true, // By default it's false, so don't forget to enable it
+
+				duration: 300, // duration of the effect, in milliseconds
+				easing: 'ease-in-out', // CSS transition easing function
+
+				// The "opener" function should return the element from which popup will be zoomed in
+				// and to which popup will be scaled down
+				// By defailt it looks for an image tag:
+				opener: function(openerElement) {
+				// openerElement is the element on which popup was initialized, in this case its <a> tag
+				// you don't need to add "opener" option if this code matches your needs, it's defailt one.
+				return openerElement.is('img') ? openerElement : openerElement.find('img');
+				}
+			}
+		});
+
+		$('.with-caption').magnificPopup({
+			type: 'image',
+			closeOnContentClick: true,
+			closeBtnInside: false,
+			mainClass: 'mfp-with-zoom mfp-img-mobile',
+			image: {
+				verticalFit: true,
+				titleSrc: function(item) {
+					return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">image source</a>';
+				}
+			},
+			zoom: {
+				enabled: true
+			}
+		});
+
+
+		$('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+      disableOn: 700,
+      type: 'iframe',
+      mainClass: 'mfp-fade',
+      removalDelay: 160,
+      preloader: false,
+
+      fixedContentPos: false
+    });
+	};
+
+	// Loading page
+	var loaderPage = function() {
+		$(".probootstrap-loader").fadeOut("slow");
+	};
+
+	var goToTop = function() {
+
+		$('.js-gotop').on('click', function(event){
+			
+			event.preventDefault();
+
+			$('html, body').animate({
+				scrollTop: $('html').offset().top
+			}, 500, 'easeInOutExpo');
+			
+			return false;
+		});
+
+		$(window).scroll(function(){
+			console.log('scrolling');
+			var $win = $(window);
+			if ($win.scrollTop() > 200) {
+				$('.js-top').addClass('active');
+			} else {
+				$('.js-top').removeClass('active');
+			}
+
+		});
+	
+	};
+
+	var transparentBg = function() {
+		$('.probootstrap---transparent-bg').hover(function(){
+			$('.probootstrap-text').addClass('semi-active');
+		}, function(){
+			$('.probootstrap-text').removeClass('semi-active');
+		});
+		$('.probootstrap---transparent-bg').click(function(){
+			$('.probootstrap-text').addClass('full-active');
+		})
+		$('body').on('mouseenter', '.probootstrap-text.full-active', function(){
+			$('.probootstrap-text').addClass('bg-semi-active');
+		});
+		$('body').on('mouseleave', '.probootstrap-text.full-active', function(){
+			$('.probootstrap-text').removeClass('bg-semi-active');
+		});
+		$('body').on('click', '.probootstrap-text.full-active', function(){
+			$('.probootstrap-text').removeClass('full-active bg-semi-active');
+		});
+	};
+
+	/*----------------------------------------
+		Document Ready 
+	----------------------------------------*/
+	$(document).ready(function(){
+		magnificPopupControl();
+		loaderPage();
+		goToTop();
+		transparentBg();
+	});
+
+	
+
+})();
